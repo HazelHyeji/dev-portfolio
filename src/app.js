ca1dd1a158ed477f10e92e0fdce12b43eba34c30
HAZEL <hyezico@outlook.com>
2018-01-18 12:24:07 -0500

    add babelrc and Nav component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PortfolioApp from './components/PortfolioApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
-ReactDOM.render(<p>Hey! I made it!!! This font works well on my app</p>, document.getElementById('app'));
+
+ReactDOM.render(<PortfolioApp/>, document.getElementById('app'));
 
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+    return(
+        <nav>
+            <ul>
+                <li>Projects <i class="fa fa-circle" aria-hidden="true"></i> </li>
+                <li>UI/UX Design <i class="fa fa-circle" aria-hidden="true"></i> </li>
+                <li>About <i class="fa fa-circle" aria-hidden="true"></i> </li>
+                <li>Contact <i class="fa fa-circle" aria-hidden="true"></i> </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
