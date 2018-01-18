6112bab0621bcd9dc08a27f938ba46f4e524e66b
HAZEL <hyezico@outlook.com>
2018-01-18 11:05:21 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+ReactDOM.render(<p>Hey! I made it!!! This font works well on my app</p>, document.getElementById('app'));
+
